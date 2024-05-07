9357dc023ea541aece28a233dd305a8c925247cd
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-07 12:03:05 -0400

    build: pages

---
+++ src/pages/Itinerary.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Itinerary = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Itinerary
---
+++ src/pages/MyRides.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyRides = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyRides
