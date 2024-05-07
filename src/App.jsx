405b0142a890c3398dd5cd1dc6f4745237a70c99
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-07 14:39:05 -0400

    CSS: nav bar fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-   <div>
+   <div className="App">
       <nav>
         <NavLink><Icons icon="list" />{/* to RideView */}</NavLink>
         <NavLink><Icons icon="favorites" />{/* to MyRides */}</NavLink>
--- src/components/RideCard.jsx
+++ src/components/RideCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RideCard = ({title, park}) => {
   return (
-    <div className='ride-wrapper wrapper flex-col'>
+    <div className='ride-card wrapper flex-col'>
       <h4>{title}</h4>
       <div className="caption">
         {park}
